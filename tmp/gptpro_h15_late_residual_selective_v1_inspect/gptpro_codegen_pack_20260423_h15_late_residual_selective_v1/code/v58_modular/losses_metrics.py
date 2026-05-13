4c903b0965fa45de8d7de6449bb9c9f16ae5180b
zheng-6662 <1574873759@qq.com>
2026-05-13 11:18:28 +0800

    Stop tracking generated artifacts

--- tmp/gptpro_h15_late_residual_selective_v1_inspect/gptpro_codegen_pack_20260423_h15_late_residual_selective_v1/code/v58_modular/losses_metrics.py
+++
@@ -1,4 +0,0 @@
-"""Compatibility re-export layer. Prefer importing from losses/metrics directly."""
-
-from .losses import *
-from .metrics import *
--- tmp/gptpro_h15_late_residual_selective_v1_inspect/gptpro_codegen_pack_20260423_h15_late_residual_selective_v1/code/v58_modular/shared.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility re-export layer. Prefer importing from paths/utils/config directly."""
-
-from .paths import *
-from .utils import *
-from .config import *
